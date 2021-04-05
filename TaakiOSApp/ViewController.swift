44b283a5bc4f5b910fb1c8e91537d88b447cf8f6
ElkanaRahadian <elkanarahadian@gmail.com>
2021-04-05 11:58:25 +0700

    Initial Commit

---
+++ TaakiOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaakiOSApp
+//
+//  Created by Elkana Antonio on 05/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
