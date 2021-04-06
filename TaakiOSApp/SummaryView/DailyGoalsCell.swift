a4f1dc75d96964f31f3eef4cee28391c24bf9f75
vivianangelaa <vivianangela1@gmail.com>
2021-04-06 23:43:49 +0700

    Commit changes

---
+++ TaakiOSApp/SummaryView/DailyGoalsCell.swift
@@ -0,0 +1,27 @@
+//
+//  DailyGoalsCell.swift
+//  TaakiOSApp
+//
+//  Created by Vivian Angela on 06/04/21.
+//
+
+import UIKit
+
+class DailyGoalsCell: UITableViewCell {
+
+    
+    @IBOutlet weak var setDailyGoalButton: UIButton!
+    
+    @IBOutlet weak var dailyGoalLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TaakiOSApp/SummaryView/StatisticsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  StatisticsCollectionViewCell.swift
+//  TaakiOSApp
+//
+//  Created by Vivian Angela on 06/04/21.
+//
+
+import UIKit
+
+class StatisticsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var statisticsImageTotalMin: UIImageView!
+    @IBOutlet weak var totalMinLabel: UILabel!
+    
+    @IBOutlet weak var statisticsImageDailyStreak: UIImageView!
+    @IBOutlet weak var dailyStreakLabel: UILabel!
+}
