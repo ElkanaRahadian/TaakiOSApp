f8c0178d79b0185e1820949eef8af9b1afe16414
Devina Ranlyca <devinar00@gmail.com>
2021-04-07 09:53:54 +0700

    Edit Focus Page congrats (UI)

--- TaakiOSApp/FocusPageCongrats/FocusPageCongrats.swift
+++ TaakiOSApp/FocusPageCongrats/FocusPageCongrats.swift
@@ -8,11 +8,21 @@
 import UIKit
 
 class FocusPageCongrats: UIViewController {
+    @IBOutlet weak var congratsImage: UIImageView!
     
+    @IBOutlet weak var congratsTitle: UILabel!
+    @IBOutlet weak var congratsDesc1: UILabel!
+    @IBOutlet weak var congratsDesk2: UILabel!
+    
+    @IBOutlet weak var congratsPoints: UILabel!
+    
+    @IBOutlet weak var congratsButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        congratsImage.image = #imageLiteral(resourceName: <#T##String#>)
         
     }
 }
