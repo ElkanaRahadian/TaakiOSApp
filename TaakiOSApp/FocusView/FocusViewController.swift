b9d282753dd89419395ee5649fddf24bb48aaebb
ElkanaRahadian <elkanarahadian@gmail.com>
2021-04-07 19:02:38 +0700

    Update FocusView

---
+++ TaakiOSApp/FocusView/FocusViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FocusViewController.swift
+//  TaakiOSApp
+//
+//  Created by Elkana Antonio on 07/04/21.
+//
+
+import UIKit
+
+class FocusViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
