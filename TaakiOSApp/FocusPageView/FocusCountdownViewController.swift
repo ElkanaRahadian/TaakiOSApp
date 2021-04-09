fe2ac5e49c7a88c369467eb3ef7366f53c34599b
ElkanaRahadian <elkanarahadian@gmail.com>
2021-04-10 03:39:40 +0700

    (Merged) Update FocusCountdown

---
+++ TaakiOSApp/FocusPageView/FocusCountdownViewController.swift
@@ -0,0 +1,68 @@
+import Foundation
+import UIKit
+
+class FocusCountdownViewController : UIViewController {
+
+    @IBOutlet weak var countdownLabel: UILabel!
+    @IBOutlet weak var startFinishButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    var timer : Timer = Timer()
+    var count : Int = 0
+    var timerCounting : Bool = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startFinishButton.setTitleColor(UIColor.green, for: .normal)
+    }
+    
+    @IBAction func cancelTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Cancel Countdown", message: "Are you sure you would like to cancel the countdown ?", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Keep Focus", style: .cancel, handler: { (_) in }))
+        alert.addAction(UIAlertAction(title: "End Focus", style: .default, handler: { (_) in
+            self.count = 0
+            self.timer.invalidate()
+            self.countdownLabel.text = self.makeTimeString(hours: 0, minutes: 0, seconds: 0)
+            self.startFinishButton.setTitle("Start", for: .normal)
+            self.startFinishButton.setTitleColor(UIColor.green, for: .normal)
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    @IBAction func startFinishButton(_ sender: Any) {
+        if (timerCounting) {
+            timerCounting = false
+            timer.invalidate()
+            startFinishButton.setTitle("Start", for: .normal)
+            startFinishButton.setTitleColor(UIColor.green, for: .normal)
+        }
+        else {
+            timerCounting = true
+            startFinishButton.setTitle("Finish", for: .normal)
+            startFinishButton.setTitleColor(UIColor.red, for: .normal)
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
+        }
+    }
+
+    @objc func timerCounter() -> Void {
+        count = count + 1
+        let time = secondsToHoursMinutesSecond(seconds: count)
+        let timeString = makeTimeString(hours: time.0, minutes: time.1, seconds: time.2)
+        countdownLabel.text = timeString
+    }
+
+    func secondsToHoursMinutesSecond(seconds : Int) -> (Int,Int, Int) {
+        return (seconds / 3600, ((seconds % 3600) / 60), ((seconds % 3600) % 60))
+    }
+
+    func makeTimeString(hours: Int, minutes: Int, seconds: Int) -> String{
+        var timeString = ""
+        timeString += String(format: "%02d", hours)
+        timeString += " : "
+        timeString += String(format: "%02d", minutes)
+        timeString += " : "
+        timeString += String(format: "%02d", seconds)
+        timeString += " : "
+        return timeString
+    }
+}
