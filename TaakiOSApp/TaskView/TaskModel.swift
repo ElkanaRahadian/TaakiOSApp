e257d72e69d3ebb90d82fea6851421614521cdc7
ElkanaRahadian <elkanarahadian@gmail.com>
2021-04-10 16:04:15 +0700

    Update FocusPage

--- TaakiOSApp/TaskView/TaskModel.swift
+++ TaakiOSApp/TaskView/TaskModel.swift
@@ -1,4 +1,10 @@
-simport Foundation
+//  TaskModel.swift
+//  TaakiOSApp
+//
+//  Created by Apriliani Putri Prasetyo on 07/04/21.
+//
+
+import Foundation
 
 struct TaskModel {
     var taskName: String
