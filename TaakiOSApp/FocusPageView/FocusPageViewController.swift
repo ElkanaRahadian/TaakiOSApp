6f094fad5cb49f0d60753608e39442c4f2dea986
vivianangelaa <vivianangela1@gmail.com>
2021-04-13 16:39:17 +0700

    change dailyGoals in FocusView (Fail)

--- TaakiOSApp/FocusPageView/FocusPageViewController.swift
+++ TaakiOSApp/FocusPageView/FocusPageViewController.swift
@@ -2,7 +2,12 @@ import Foundation
 import UIKit
 
 class FocusPageViewController : UIViewController {
+    @IBOutlet weak var dailyGoalDetailLabel: UILabel!
+    
+//    let passValue = SummaryViewController.valueDailyGoal
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
 }
